a7d079ddd87b57689cc2c98982b3b605b37ddf7c
서지운 <96479626+MildColor@users.noreply.github.com>
2024-05-08 14:35:15 +0900

    feat: ChatRoom 컴포넌트 표출 로직 수정

--- src/components/chat/ChatRoom.tsx
+++ src/components/chat/ChatRoom.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import ChatLayout from "./ChatLayout";
 import ChatTextBox from "./ChatTextBox";
 import ChatInputWithButton from "./ChatInputWithButton";
+import useGetMusicInfo from "@/hooks/useGetMusicInfo";
 
 const ChatRoom = () => {
+  const { data: musicInfo } = useGetMusicInfo();
+
   return (
-    <ChatLayout>
-      <ChatTextBox type="me" text="hi" />
-      <ChatInputWithButton />
-    </ChatLayout>
+    <>
+      {musicInfo?.isVisibility && (
+        <>
+          <ChatLayout>
+            <ChatTextBox type="me" text="hi" />
+            <ChatInputWithButton />
+          </ChatLayout>
+        </>
+      )}
+    </>
   );
 };
 
